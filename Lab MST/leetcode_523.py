7e0bb909e65c2bdb4926c5935277769d9ff0764f
Nandesh <nandeshmontfort@gmail.com>
2026-03-23 20:03:54 +0530

    Implement checkSubarraySum method in leetcode_523.py

---
+++ Lab MST/leetcode_523.py
@@ -0,0 +1,16 @@
+class Solution:
+    def checkSubarraySum(self, nums, k):
+        remainder_map = {0: -1}
+        prefix_sum = 0
+        
+        for i, num in enumerate(nums):
+            prefix_sum += num
+            remainder = prefix_sum % k
+            
+            if remainder in remainder_map:
+                if i - remainder_map[remainder] > 1:
+                    return True
+            else:
+                remainder_map[remainder] = i
+        
+        return False
